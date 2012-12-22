d2bb3f725deccd19bc32f7be42b5e2d659a9e54f
George Opritescu <ssscripting@gmail.com>
2012-12-22 14:04:59 +0200

    Adjust timer to 25 minutes, and added icon to notification

--- background.js
+++ background.js
@@ -4,7 +4,7 @@
 
   pomodorro_running = false;
 
-  default_minutes = 25;
+  default_minutes = 1;
 
   time = default_minutes * 60;
 
@@ -40,7 +40,7 @@
     time -= 1;
     if (time < 0) {
       pomodorro_running = false;
-      notif = webkitNotifications.createNotification(null, "Pomodorro finished", "Congrats!");
+      notif = webkitNotifications.createNotification("icon.png", "Pomodorro finished", "Congrats!");
       return notif.show();
     } else {
       return window.setTimeout(click_listener, 1000);
